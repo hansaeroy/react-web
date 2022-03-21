457d67b986b5380cd1b06524e04f88d437859eef
hansaeroy <hyc0331@gmail.com>
2022-03-21 12:54:46 +0900

    Styled Component 작업

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import CardItem from './CardItem';
 import styled from 'styled-components';
+import img2 from '../assets/img-2.jpg';
+import img3 from '../assets/img-3.jpg';
+import img4 from '../assets/img-4.jpg';
+import img8 from '../assets/img-8.jpg';
+import img9 from '../assets/img-9.jpg';
 
 const CardBlock = styled.div`
   padding: 4rem;
@@ -42,13 +47,13 @@ const Cards = () => {
         <Wrapper>
           <ul>
             <CardItem
-              src={process.env.PUBLIC_URL + 'images/img-9.jpg'}
+              src={img9}
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
               label='Adventure'
               path='/services'
             />
             <CardItem
-              src={process.env.PUBLIC_URL + 'images/img-2.jpg'}
+              src={img2}
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path='/services'
@@ -56,19 +61,19 @@ const Cards = () => {
           </ul>
           <ul>
             <CardItem
-              src={process.env.PUBLIC_URL + 'images/img-3.jpg'}
+              src={img3}
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src={process.env.PUBLIC_URL + 'images/img-4.jpg'}
+              src={img4}
               text='Experience Football on Top of the Himilayan Mountains'
               label='Adventure'
               path='/products'
             />
             <CardItem
-              src={process.env.PUBLIC_URL + 'images/img-8.jpg'}
+              src={img8}
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
               path='/sign-up'
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,119 +1,265 @@
 import React from 'react';
-import './Footer.css';
 import Button from './Button';
 import { Link } from 'react-router-dom';
+import styled, { css } from 'styled-components';
 
+const FooterBlock = styled.div`
+  background-color: #242424;
+  padding: 4rem 0 2rem 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Subscription = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin-bottom: 24px;
+  padding: 24px;
+  color: #fff;
+  @media screen and (max-width: 820px) {
+    Button {
+      width: 100%;
+    }
+  }
+`;
+
+const Text = styled.p`
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
+    'Lucida Sans', Arial, sans-serif;
+  margin-bottom: 24px;
+
+  ${(props) =>
+    props.head &&
+    css`
+      font-size: 24px;
+    `}
+
+  ${(props) =>
+    props.text &&
+    css`
+      font-size: 20px;
+    `}
+`;
+
+const FooterInput = styled.input`
+  padding: 8px 20px;
+  border-radius: 2px;
+  margin-right: 10px;
+  outline: none;
+  border: none;
+  font-size: 18px;
+  margin-bottom: 16px;
+  border: 1px solid #fff;
+
+  &::placeholder {
+    color: #b1b1b1;
+  }
+
+  @media screen and (max-width: 820px) {
+    width: 100%;
+  }
+`;
+
+const LinkBlock = styled.div`
+  width: 100%;
+  max-width: 1000px;
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 820px) {
+    padding-top: 2rem;
+  }
+`;
+
+const LinkWrap = styled.div`
+  display: flex;
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+  }
+`;
+
+const LinkItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 16px;
+  text-align: left;
+  width: 160px;
+  box-sizing: border-box;
+
+  h2 {
+    margin-bottom: 16px;
+    color: #fff;
+  }
+
+  a {
+    color: #fff;
+    text-decoration: none;
+    margin-bottom: 0.5rem;
+
+    &:hover {
+      color: #a8a8a8;
+      transition: 0.3s ease-out;
+    }
+  }
+`;
+
+const SocialBlock = styled.section`
+  max-width: 1000px;
+  width: 100%;
+`;
+
+const SocialWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  max-width: 980px;
+  margin: 40px auto 0 auto;
+
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+  }
+`;
+
+const FooterLogo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LogoLink = styled(Link)`
+  color: #fff;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SocialIcon = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 240px;
+`;
+
+const SocialLink = styled(Link)`
+  color: #fff;
+  font-size: 24px;
+`;
+
+const Copyright = styled.small`
+  color: #fff;
+`;
 function Footer() {
   return (
-    <div className='footer-container'>
-      <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
+    <FooterBlock>
+      <Subscription>
+        <Text head>
           Join the Adventure newsletter to receive our best vacation deals
-        </p>
-        <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
-        </p>
-        <div className='input-areas'>
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-            <Button outline={true}>Subscribe</Button>
-          </form>
-        </div>
-      </section>
-      <div className='footer-links'>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
+        </Text>
+        <Text text>You can unsubscribe at any time.</Text>
+        <form>
+          <FooterInput name='email' type='email' placeholder='Your Email' />
+          <Button outline={true}>Subscribe</Button>
+        </form>
+      </Subscription>
+      <LinkBlock>
+        <LinkWrap>
+          <LinkItems>
             <h2>About Us</h2>
             <Link to='/sign-up'>How it works</Link>
             <Link to='/'>Testimonials</Link>
             <Link to='/'>Careers</Link>
             <Link to='/'>Investors</Link>
             <Link to='/'>Terms of Service</Link>
-          </div>
-          <div className='footer-link-items'>
+          </LinkItems>
+          <LinkItems>
             <h2>Contact Us</h2>
             <Link to='/'>Contact</Link>
             <Link to='/'>Support</Link>
             <Link to='/'>Destinations</Link>
             <Link to='/'>Sponsorships</Link>
-          </div>
-        </div>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
+          </LinkItems>
+        </LinkWrap>
+        <LinkWrap>
+          <LinkItems>
             <h2>Videos</h2>
             <Link to='/'>Submit Video</Link>
             <Link to='/'>Ambassadors</Link>
             <Link to='/'>Agency</Link>
             <Link to='/'>Influencer</Link>
-          </div>
-          <div className='footer-link-items'>
+          </LinkItems>
+          <LinkItems>
             <h2>Social Media</h2>
             <Link to='/'>Instagram</Link>
             <Link to='/'>Facebook</Link>
             <Link to='/'>Youtube</Link>
             <Link to='/'>Twitter</Link>
-          </div>
-        </div>
-      </div>
-      <section className='social-media'>
-        <div className='social-media-wrap'>
-          <div className='footer-logo'>
-            <Link to='/' className='social-logo'>
-              TRVL
+          </LinkItems>
+        </LinkWrap>
+      </LinkBlock>
+      <SocialBlock>
+        <SocialWrap>
+          <FooterLogo>
+            <LogoLink to='/'>
+              LOGO
               <i className='fab fa-typo3' />
-            </Link>
-          </div>
-          <small className='website-rights'>TRVL © 2022</small>
-          <div className='social-icons'>
-            <Link
-              className='social-icon-link facebook'
+            </LogoLink>
+          </FooterLogo>
+          <Copyright>TRVL © 2022</Copyright>
+          <SocialIcon>
+            <SocialLink
+              className='facebook'
               to='/'
               target='_blank'
               aria-label='Facebook'
             >
               <i className='fab fa-facebook-f' />
-            </Link>
-            <Link
-              className='social-icon-link instagram'
+            </SocialLink>
+            <SocialLink
+              className='instagram'
               to='/'
               target='_blank'
               aria-label='Instagram'
             >
               <i className='fab fa-instagram' />
-            </Link>
-            <Link
-              className='social-icon-link youtube'
+            </SocialLink>
+            <SocialLink
+              className='youtube'
               to='/'
               target='_blank'
               aria-label='Youtube'
             >
               <i className='fab fa-youtube' />
-            </Link>
-            <Link
-              className='social-icon-link twitter'
+            </SocialLink>
+            <SocialLink
+              className='twitter'
               to='/'
               target='_blank'
               aria-label='Twitter'
             >
               <i className='fab fa-twitter' />
-            </Link>
-            <Link
-              className='social-icon-link twitter'
+            </SocialLink>
+            <SocialLink
+              className='twitter'
               to='/'
               target='_blank'
               aria-label='LinkedIn'
             >
               <i className='fab fa-linkedin' />
-            </Link>
-          </div>
-        </div>
-      </section>
-    </div>
+            </SocialLink>
+          </SocialIcon>
+        </SocialWrap>
+      </SocialBlock>
+    </FooterBlock>
   );
 }
 
